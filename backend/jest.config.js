f953eaaff517e3ed0f97bd18c9fcec18e88fdb52
zhang_jq <zhangjq@fuchart.com>
2026-05-24 11:05:46 +0800

    chore(scan): add jest configuration and tests directory

---
+++ backend/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
+};
